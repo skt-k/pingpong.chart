64bb517c57cd6ebb9ec31789dc203364337e2e8b
Sanpipop Batriya <sanpipop9@gmail.com>
2024-02-22 14:11:40 +0700

    start app

--- PPC.py
+++ PPC.py
@@ -1,1 +1,18 @@
-print(1)
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.graphics import Rectangle
+
+class GameWidget(Widget):
+    def __init__(self,**kwargs):
+        super().__init__(**kwargs)
+        with self.canvas:
+            Rectangle(pos=(0,0),size=(100,100))
+
+class PccApp(App):
+    def build(self):
+        return GameWidget()
+
+if __name__ == '__main__':
+    app = PccApp()
+    app.run()
+
