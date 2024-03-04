f69d246b459b6f728466fac4291d6a61cbb6d309
Sanpipop Batriya <sanpipop9@gmail.com>
2024-03-04 23:08:54 +0700

    ลองใส่ตัวละคร

--- PPC.py
+++ PPC.py
@@ -8,24 +8,20 @@ class GameWidget(Widget):
         super().__init__(**kwargs)
         self._keyboard = Window.request_keyboard(self._on_keyboard_closed,self)
         self._keyboard.bind(on_key_down=self._on_key_down)
-
-        with self.canvas:
-            Rectangle(pos=(0,0),size=(100,100))
-
+        
     def _on_keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_key_down)
         self._keyboard = None
 
     def _on_key_down(self,keyboard,keycode,text,modifiers):
-        print('you click ',text,' button')
+        print('you click ',text,' button') 
 
-        
-
-class PccApp(App):
+    
+class PpcApp(App):
     def build(self):
         return GameWidget()
 
 if __name__ == '__main__':
-    app = PccApp()
+    app = PpcApp()
     app.run()
 
