f26824dfa248bd5e41776436d91c14ebd0099a69
Sanpipop Batriya <sanpipop9@gmail.com>
2024-03-08 10:43:16 +0700

    ระบบพลังงาน

--- PPC.py
+++ PPC.py
@@ -1,15 +1,16 @@
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.properties import NumericProperty, ReferenceListProperty,ObjectProperty
-
 from kivy.core.window import Window
 
 
 class Player(Widget):
+    energy = NumericProperty(3)
     def attack(self):
         pass
     
 class Enemy(Widget):
+    energy = NumericProperty(3)
     def attack(self):
         pass
 
@@ -19,16 +20,21 @@ class GameWidget(Widget):
     
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
-        self._keyboard = Window.request_keyboard(
-            self._on_keyboard_closed, self)
+        self._keyboard = Window.request_keyboard(self._on_keyboard_closed,self)
         self._keyboard.bind(on_key_down=self._on_key_down)
         
     def _on_keyboard_closed(self):
         self._keyboard.unbind(on_key_down=self._on_key_down)
         self._keyboard = None
 
-    def _on_key_down(self, keyboard, keycode, text, modifiers):
+    def _on_key_down(self,keyboard,keycode,text,modifiers):
         print('you press ', text)
+        if text == 'j':
+            self.player.energy += 1
+            self.enemy.energy += 1
+        if text == 'k':
+            self.player.energy -= 1
+            self.enemy.energy -= 1
 
 
 
